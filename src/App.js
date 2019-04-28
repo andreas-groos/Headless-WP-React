fd1423c1fae800ad0750d4637405d486621de39d
Andreas Groos <andreas.groos1@gmail.com>
2019-04-28 06:46:59 -0700

    Workinng

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    posts: null,
+    post: null
+  }
+  componentDidMount = () => {
+    axios.get('https://yubanet.com/wp-json/wp/v2/posts').then(res => {
+      console.log('res', res)
+      this.setState({ posts: res.data })
+    })
+  }
+
   render() {
+    const { posts, post } = this.state || []
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 onClick={() => this.setState({ post: null })}>YubaNet Clone</h1>
+          {post ? <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div> :
+            posts && posts.map((p, i) => <h3 key={i} onClick={() => this.setState({ post: p })}>{p.title.rendered}</h3>)}
         </header>
       </div>
     );
